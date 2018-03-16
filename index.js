e0485c1cc4d304769d5118ee154698c29e858f1b
icet00 <florent.enaux@gmail.com>
2018-03-16 19:15:38 +0100

    feat: added the populate function for all value

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 var fs = require('fs');
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+	host: 'localhost:9200',
+	log: 'trace'
+});
 
 //API configuration
 var express = require('express'),
-  app = express(),
-  port = 9292;
+app = express(),
+port = 9292;
 
 async function f1() {
-  var brands = await getBrands();
-  console.log(brands);
+	var brands = await getBrands();
+	console.log(brands);
 }
 //f1();
 
+function insertWithBulk(toString)
+{
+	client.bulk({
+		body: [toString]
+	}, function (err, resp) {
+	  // ...
+	});
+}
+
 //Populate function
 async function populate () {
-  const models = await getModels('PEUGEOT');
-  var toString = "";
-  var id = 0;
-
-  models.forEach(function(element) {
-
-  		toString += '{ "index":{ "_id": "'+id+'"} }\n';
-		toString += JSON.stringify(element)+"\n";
-		id++;
+	var brands = await getBrands();
+	var toString = "";
+	var id = 0;
+	for(var i =0; i < 30; i++)//Change to 30 to brands.length but it is really long
+	{
+		var brand = brands[i];
+		if(!(typeof brand === "undefined"))
+		{
+			var models = await getModels(brand);
+			models.forEach(function(element)
+			{
+				console.log(element);
+				toString += '{ "index":{ "_index": "suv", "_type":"suv", "_id": "'+id+'"} }\n';
+				toString += JSON.stringify(element)+"\n";
+				id++;
+			});			
+		}
+		console.log("percentage " + (i/30) + "%");
+	}
+	console.log("finished the loading data");
+	fs.writeFile('value.json', toString, function (err) {
+		if (err) throw err;
 	});
-
-  fs.writeFile('value.json', toString, function (err) {
-    if (err) throw err;
-  });
+	console.log("Writing finished");
+	insertWithBulk(toString);
 }
 
 //API route
 app.get('/', (req, res) => {
     res.send('Hello');
+});
+
+app.post('/populate', (req, res) => {
+	populate();
+    res.send('Populate elasticsearch done');
 });
 
 app.listen(port);
